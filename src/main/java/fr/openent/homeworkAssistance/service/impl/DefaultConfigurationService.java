4085dbfb907ef71b72f07ab5f7b8a713394beac9
adrien.frossard <adrien.frossard@cgi.com>
2022-07-12 17:52:24 +0200

    fix(): #CO-1654 fix of config duplicate

--- src/main/java/fr/openent/homeworkAssistance/service/impl/DefaultConfigurationService.java
+++ src/main/java/fr/openent/homeworkAssistance/service/impl/DefaultConfigurationService.java
@@ -1,12 +1,20 @@
 package fr.openent.homeworkAssistance.service.impl;
 
+import com.mongodb.QueryBuilder;
 import fr.openent.homeworkAssistance.service.IConfigurationService;
+import fr.wseduc.mongodb.MongoQueryBuilder;
 import fr.wseduc.webutils.Either;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.entcore.common.service.impl.MongoDbCrudService;
 
+import static org.entcore.common.mongodb.MongoDbResult.validActionResultHandler;
+
 public class DefaultConfigurationService extends MongoDbCrudService implements IConfigurationService {
+
+    private static final Logger log = LoggerFactory.getLogger(DefaultConfigurationService.class);
 
     public DefaultConfigurationService(String collection) {
         super(collection);
@@ -16,24 +24,43 @@ public class DefaultConfigurationService extends MongoDbCrudService implements I
     public void get(Handler<Either<String, JsonObject>> handler) {
         mongo.findOne(collection, new JsonObject(), event -> {
             JsonObject result = new JsonObject();
-            if(event.body().containsKey("result"))
+            if(event.body().containsKey("result")){
                 result = event.body().getJsonObject("result");
-            if(result.containsKey("messages"))
+            }
+            if(result.containsKey("messages")){
                 handler.handle(new Either.Right(result));
-            else
-            handler.handle(new Either.Left<>("No waiting orders"));
+
+            }
+            else{
+                handler.handle(new Either.Left<>("No waiting orders"));
+            }
         });
     }
 
     @Override
     public void update(Handler<Either<String, JsonObject>> handler, JsonObject settings) {
-        mongo.findOne(collection, new JsonObject(), result -> {
-            mongo.save(collection, settings, event -> {
-                if(event.body().getString("status").equals("ok"))
+
+        mongo.save(collection, settings, event -> {
+            if (event.body().getString("status").equals("ok")){
+                log.info("[Homework-assistance@Config] La configuration a été sauvegardé");
+                String id = settings.getString("_id");
+                QueryBuilder query = QueryBuilder.start("_id").notEquals(id);
+                mongo.delete(collection, MongoQueryBuilder.build(query), validActionResultHandler(handler));
+                if(event.body().getString("status").equals("ok")){
                     handler.handle(new Either.Right(settings));
-                else
-                    handler.handle(new Either.Left<>("duplicated"));
-            });
+                }
+                else {
+                    String message = "[Homework-assistance@Config] Erreur lors de la suppression";
+                    log.error(message);
+                    handler.handle(new Either.Left<>(message));
+                }
+
+            }
+            else {
+                String message = "[Homework-assistance@Config] Erreur lors de l'enregistrement de la modification";
+                log.error(message);
+                handler.handle(new Either.Left<>(message));
+            }
         });
     }
 
