82a59366d4a7a4f761d5bf8e5fb97a7f88271d84
jinhda douang <s>
2022-07-12 22:12:43 +0200

    fix(date): #CO-1657 fix delay date timezone

--- src/test/java/fr/openent/homeworkAssistance/models/KiamoFormTest.java
+++ src/test/java/fr/openent/homeworkAssistance/models/KiamoFormTest.java
@@ -50,4 +50,44 @@ public class KiamoFormTest {
 
         ctx.assertEquals(new JsonArray(expectedPayload), kiamoForm.homeworkAssistanceToKiamo());
     }
+
+    @Test
+    public void homeworkAssistanceShould_Send_Correct_Payload_With_Another_Format_Date(TestContext ctx) {
+        JsonObject homeworkCallbackForm = new JsonObject(
+                "{" +
+                        "\"destination\":\"06000000\"," +
+                        "\"callback_date\":\"2022-07-14T00:00:00Z\"," +
+                        "\"callback_time\":{" +
+                        "    \"hour\":\"11\"," +
+                        "    \"minute\":15" +
+                        "}," +
+                        "\"userdata\":{" +
+                        "   \"prenom\":\"Miradi\"," +
+                        "   \"nom\":\"BON\"," +
+                        "   \"etablissement\":\"Etablissement Formation 15613\"," +
+                        "   \"classe\":\"6 3\"," +
+                        "   \"matiere\":\"Test\"," +
+                        "   \"service\":251\r\n   " +
+                        "}," +
+                        "\"informations_complementaires\":\"test\"" +
+                        "}");
+        KiamoForm kiamoForm = new KiamoForm(homeworkCallbackForm);
+
+        String expectedPayload = "[" +
+                "{" +
+                "\"destination\": \"06000000\"," +
+                "\"callback_date\": \"2022-07-14T11:15:00+02:00\"," +
+                "\"userdata\": {" +
+                "    \"prenom_eleve\": \"Miradi\"," +
+                "    \"nom_eleve\": \"BON\"," +
+                "    \"etablissement\": \"Etablissement Formation 15613\"," +
+                "    \"classe\": \"6 3\"," +
+                "    \"matiere_aide\": \"Test\"," +
+                "    \"informations_complementaires\": \"test\"" +
+                " }" +
+                " }" +
+                "]";
+
+        ctx.assertEquals(new JsonArray(expectedPayload), kiamoForm.homeworkAssistanceToKiamo());
+    }
 }
