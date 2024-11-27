ce6f3a4bad94eb7915ff5f3c704ed4079c9682cb
Thomas Sevagen <thomas.sevagen@cgi.com>
2024-11-27 19:47:57 +0100

    chore(init): wip

---
+++ frontend/src/core/types.ts
@@ -0,0 +1,13 @@
+import { ID } from "edifice-ts-client";
+
+export interface AppProps {
+  _id: string;
+  created: Date;
+  description: string;
+  map: string;
+  modified: Date;
+  name: string;
+  owner: { userId: ID; displayName: string };
+  shared: any[];
+  thumbnail: string;
+}
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { createHashRouter } from "react-router-dom";
+
+import Root from "~/app/root";
+import ErrorPage from "~/components/page-error";
+import "~/styles/index.scss";
+
+const routes = [
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        async lazy() {
+          const { App } = await import("./app");
+          return {
+            Component: App,
+          };
+        },
+      },
+    ],
+  },
+];
+
+export const router = createHashRouter(routes);
