85f608acff221f92b6f40c60ab8b78bc03abcdb1
Quentin Périé <qper@FR-L8CKSHL3.groupinfra.com>
2025-01-22 14:25:20 +0100

    feat(): add config route

--- backend/src/main/java/fr/openent/homeworkAssistance/controller/HomeworkAssistanceController.java
+++ backend/src/main/java/fr/openent/homeworkAssistance/controller/HomeworkAssistanceController.java
@@ -2,9 +2,12 @@ package fr.openent.homeworkAssistance.controller;
 
 import java.util.Map;
 
+import fr.wseduc.security.ActionType;
 import org.entcore.common.controller.ControllerHelper;
 import org.entcore.common.events.EventStore;
 import org.entcore.common.events.EventStoreFactory;
+import org.entcore.common.http.filter.ResourceFilter;
+import org.entcore.common.http.filter.SuperAdminFilter;
 import org.vertx.java.core.http.RouteMatcher;
 
 import fr.openent.homeworkAssistance.HomeworkAssistance;
@@ -13,6 +16,9 @@ import fr.wseduc.security.SecuredAction;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
+
+import static fr.openent.homeworkAssistance.core.constants.Field.KEY;
+import static fr.openent.homeworkAssistance.core.constants.Field.KIAMO;
 
 public class HomeworkAssistanceController extends ControllerHelper {
     private EventStore eventStore;
@@ -34,5 +40,19 @@ public class HomeworkAssistanceController extends ControllerHelper {
     public void view (HttpServerRequest request) {
         renderView(request, new JsonObject(),"index.html", null);
         eventStore.createAndStoreEvent(HomeworkAssistanceEvent.ACCESS.name(), request);
+    }
+
+    @Get("/config-infos")
+    @SecuredAction(value = "", type = ActionType.RESOURCE)
+    @ResourceFilter(SuperAdminFilter.class)
+    public void getConfig(final HttpServerRequest request) {
+        JsonObject safeConfig = config.copy();
+
+        JsonObject kiamo = safeConfig.getJsonObject(KIAMO, null);
+        if (kiamo != null) {
+            if (kiamo.getString(KEY, null) != null) kiamo.put(KEY, "**********");
+        }
+
+        renderJson(request, safeConfig);
     }
 }
