32f150d289ec2bb7be120bb435cedc1fb9fefdea
Quentin Périé <qper@FR-L8CKSHL3.groupinfra.com>
2025-01-22 11:05:01 +0100

    chore(init): setup react frontend

--- src/main/resources/public/ts/behaviours.ts
+++
@@ -1,6 +0,0 @@
-import { Behaviours } from 'entcore';
-import rights from "./rights";
-
-Behaviours.register('homework-assistance', {
-    rights
-});
--- src/main/resources/public/ts/rights.ts
+++
@@ -1,8 +0,0 @@
-const rights = {
-    workflow: {
-        admin: 'fr.openent.homeworkAssistance.controller.ConfigurationController|update',
-        student: 'fr.openent.homeworkAssistance.controller.CallbackController|send'
-    }
-};
-
-export default rights;
